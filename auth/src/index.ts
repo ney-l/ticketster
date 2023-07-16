4c08426961b28644a1933b96b898fd871abd9bf2
Neha Demirci <neylanjewar@gmail.com>
2023-07-16 19:06:47 +0200

    update service name to be same as used in auth-depl

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import { json } from 'body-parser';
 
 const PORT = 3000;
+const SERVICE_NAME = 'tk-auth-srv';
 
 const app = express();
 
 app.use(json());
 
 app.listen(PORT, () => {
-  console.log(`Listening on http:localhost:${PORT} ⚡️`);
+  console.log(`Listening on http://${SERVICE_NAME}:${PORT} ⚡️`);
 });
