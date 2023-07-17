3b0555c0b2893af7297d52f837e27434295f0df7
Neha Demirci <neylanjewar@gmail.com>
2023-07-17 16:10:06 +0200

    logs errors

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,5 @@
 import { CustomError, GenericError } from '@/errors';
+import logger from '@/logger';
 import { type NextFunction, type Request, type Response } from 'express';
 
 export const errorHandler = (
@@ -8,9 +9,17 @@ export const errorHandler = (
   next: NextFunction,
 ) => {
   if (err instanceof CustomError) {
+    if (err.statusCode >= 500) {
+      logErrors(err);
+    }
     return res.status(err.statusCode).json(err.serializeErrors());
   }
 
+  logErrors(err);
   const error = new GenericError(err.message);
   res.status(error.statusCode).json(error.serializeErrors());
+};
+
+const logErrors = (err: Error) => {
+  logger.error(err.stack);
 };
