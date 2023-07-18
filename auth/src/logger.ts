812b9406509e8cbeb3cee609aaf152828278566d
Neha Demirci <neylanjewar@gmail.com>
2023-07-18 17:10:54 +0200

    turn off logging in test environment

--- auth/src/logger.ts
+++ auth/src/logger.ts
@@ -10,16 +10,30 @@ const colors = {
 
 winston.addColors(colors);
 
+const getLogLevel = () => {
+  switch (process.env.NODE_ENV) {
+    case 'production':
+      return 'error';
+
+    case 'test':
+      return 'none';
+
+    default:
+      return 'debug';
+  }
+};
+const level = getLogLevel();
+
 // Create a Winston logger instance
 const logger = winston.createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+  level,
   transports: [new winston.transports.Console()],
   format: winston.format.combine(
     winston.format.colorize({ all: true }),
     winston.format.timestamp(),
-    winston.format.printf(({ level, message, timestamp }) => {
+    winston.format.printf(({ message }) => {
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+      return `[${Date.now()}] ${level.toUpperCase()}: ${message}`;
     }),
   ),
 });
