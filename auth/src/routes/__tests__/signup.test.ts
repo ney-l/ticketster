4f9c5ff8c01d9daf04d345ba8376476608f9f962
Neha Demirci <neylanjewar@gmail.com>
2023-07-18 17:14:03 +0200

    removed sanity test

--- auth/src/routes/__tests__/signup.test.ts
+++ auth/src/routes/__tests__/signup.test.ts
@@ -2,8 +2,6 @@ import request from 'supertest';
 import { app } from '@/app';
 
 it('returns a 201 on successful signup', async () => {
-  expect(2).toBe(2);
-
   return await request(app)
     .post('/api/users/signup')
     .send({
