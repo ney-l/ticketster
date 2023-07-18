ac1601c63959baea9316845bacac8264a53cef79
Neha Demirci <neylanjewar@gmail.com>
2023-07-18 16:02:39 +0200

    add error handling for missing or invalid token

--- auth/src/middlewares/attach-current-user.ts
+++ auth/src/middlewares/attach-current-user.ts
@@ -1,6 +1,7 @@
 import { type UserPayload } from '@/types';
 import { verifyJwt } from '@/services';
 import { type Request, type Response, type NextFunction } from 'express';
+import { BadRequestError, NotAuthorizedError } from '@/errors';
 
 export const attachCurrentUser = (
   req: Request,
@@ -8,14 +9,14 @@ export const attachCurrentUser = (
   next: NextFunction,
 ) => {
   if (!req.session?.jwt) {
-    return next();
+    throw new BadRequestError('Missing Authentication Token.');
   }
 
   try {
     const payload = verifyJwt(req.session.jwt) as UserPayload;
     req.currentUser = payload;
   } catch (err) {
-    // error will be caught further down the chain
+    throw new NotAuthorizedError();
   } finally {
     next();
   }
