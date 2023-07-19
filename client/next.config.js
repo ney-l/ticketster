99f6696512778ac5a14264e41357c3bf85306319
Neha Demirci <neylanjewar@gmail.com>
2023-07-19 16:54:11 +0200

    fix: image rebuilding instead of files syncing

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-POLL_INTERVAL_IN_MILLISECONDS = 300;
-
 const nextConfig = {
-  webpack: (config) => {
-    if (config.watchOptions.poll) {
-      config.watchOptions.poll = POLL_INTERVAL_IN_MILLISECONDS;
-    }
-    return config;
-  },
+  output: 'standalone',
 };
 
 module.exports = nextConfig;
