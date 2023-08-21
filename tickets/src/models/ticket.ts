997bac0e027ec8fc7ded918bcdd225a91cf7093c
Neha Demirci <neylanjewar@gmail.com>
2023-08-21 08:35:22 +0200

    feat: add optimistic concurrency control to Mongoose

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   title: string;
@@ -12,6 +13,7 @@ interface TicketDoc extends mongoose.Document {
   userId: string;
   createdAt: string;
   updatedAt: string;
+  version: number;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -44,6 +46,17 @@ const ticketSchema = new mongoose.Schema(
     timestamps: true,
   },
 );
+
+/**
+ * Rename __v to version
+ */
+ticketSchema.set('versionKey', 'version');
+/**
+ * This plugin is used to update the version number of a document when it is
+ * updated. This is useful when we want to use optimistic concurrency control
+ * to prevent multiple users from updating the same document at the same time.
+ */
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
